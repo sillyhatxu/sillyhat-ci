0f7d442919754c520bcc7b857353e1ea6632ba31
xushikuan <xushikuan1@163.com>
2017-11-19 22:24:35 +0800

    add create and clean shell

--- shell/sillyhat/cloud/docker-service.create.sh
+++ shell/sillyhat/cloud/docker-service.create.sh
@@ -1,5 +1,18 @@
 DOCKERFILENAME=${3:-all}
 
+if [ $DOCKERFILENAME == "all" ] || [ $DOCKERFILENAME == "zuul" ]
+then
+    echo "zuul start"
+    docker service create --name zuul \
+     --replicas 1 \
+     --detach \
+     --network $1 \
+     --with-registry-auth \
+     --env SPRING_PROFILES_ACTIVE=$1 \
+     -p 8080:8080 \
+     111909622691.dkr.ecr.ap-southeast-1.amazonaws.com/zuul:$2
+fi
+
 if [ $DOCKERFILENAME == "all" ] || [ $DOCKERFILENAME == "eureka" ]
 then
     echo "eureka start"
